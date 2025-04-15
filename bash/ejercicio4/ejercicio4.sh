40c200e9af67e320210ab9b708dc19ca2a4418f5
agusPassa <aguspassa@hotmail.com>
2025-04-15 01:49:27 +0000

    Agrego funcionalidad como daemon. Se ejecuta siempre en segundo plano y hay que matarlo con un pkill ejercicio4.sh.

--- bash/ejercicio4/ejercicio4.sh
+++ bash/ejercicio4/ejercicio4.sh
@@ -32,6 +32,41 @@ function validaciones(){
     fi
 }
 
+function ordenarArchivosPorExtension(){
+    while true;do
+        #archivo=`find "$directorio" -maxdepth 1 -type f ` 
+        #No se puede hacer con find porque necesitamos que las rutas con espacios sean un solo elemento del array
+        mapfile -t archivos < <(find $directorio -maxdepth 1 -type f) #el maxdepth es para que no siga buscando adentro de las carpetas el find
+
+        num=0
+        while [[ num -lt ${#archivos[@]} ]]
+        do
+            IFS='.' read -r -a archivoActual <<< "${archivos[num]}" #El array archivoActual tiene en su posicion 1 el pathing y en el 2 la extension
+            (( num += 1 ))
+            prueba=$(find $directorio -name "${archivoActual[2]}" -type d)
+            `mkdir -p "$directorio"\/"${archivoActual[2]}"` #con el -p, mkdir no tira un error si la carpeta ya existe por lo que no necesito validar
+
+            `mv ".${archivoActual[1]}"."${archivoActual[2]}" -t "$directorio"\/"${archivoActual[2]}"` #mueve el archivo actual al directorio indicado con -t, sin el -t solo cambia el nombre
+        done
+        sleep 10
+    done
+}
+
+function validarKill(){
+    # Obtener el PID del proceso del script excluyendo el propio
+    validaProceso=$(pgrep -f "ejercicio4.sh" | grep -v $$)
+    echo $validaProceso
+    if [[ -z $validaProceso ]]; then
+        # Si no se encuentra el proceso, inicia el script en segundo plano
+        echo "El proceso no estaba iniciado, iniciando..."
+
+    else
+        # Si se encuentra el proceso, lo mata
+        echo "El proceso ya está siendo ejecutado. Procediendo a matarlo..."
+        kill $validaProceso
+    fi
+}
+
 options=$(getopt -o d:s:khc: --long help,directorio:,salida:,cantidad:,kill -- "$@" 2> /dev/null)
 if [ "$?" != "0" ] 
 then
@@ -84,17 +119,11 @@ done
 
 validaciones $directorio $salida $cantidad $kill
 
-#archivo=`find "$directorio" -maxdepth 1 -type f ` 
-#No se puede hacer con find porque necesitamos que las rutas con espacios sean un solo elemento del array
-mapfile -t archivos < <(find $directorio -maxdepth 1 -type f) #el maxdepth es para que no siga buscando adentro de las carpetas el find
+#validarKill $kill
 
-num=0
-while [[ num -lt ${#archivos[@]} ]]
-do
-    IFS='.' read -r -a archivoActual <<< "${archivos[num]}" #El array archivoActual tiene en su posicion 1 el pathing y en el 2 la extension
-    (( num += 1 ))
-    prueba=$(find $directorio -name "${archivoActual[2]}" -type d)
-    `mkdir -p "$directorio"\/"${archivoActual[2]}"` #con el -p, mkdir no tira un error si la carpeta ya existe por lo que no necesito validar
+#El disown hace que el proceso se siga ejecutando en segundo plano y me libera la terminal
+ordenarArchivosPorExtension & disown
 
-    `mv ".${archivoActual[1]}"."${archivoActual[2]}" -t "$directorio"\/"${archivoActual[2]}"` #mueve el archivo actual al directorio indicado con -t, sin el -t solo cambia el nombre
-done
+
+
+
